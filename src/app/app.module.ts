c4e7e137dd51197590ce3142e052ef982681931e
karishma <karishma.p094@gmail.com>
2022-05-14 18:04:14 -0400

    updating mat commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,17 @@ import { AppComponent } from './app.component';
 import { MortgageCalculatorComponent } from './mortgage-calculator/mortgage-calculator.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCommonModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CurrencyMaskModule } from "ng2-currency-mask";
 import { DecimalDirectiveDirective } from './shared/directives/decimal-directive/decimal-directive.directive';
@@ -25,10 +35,19 @@ import { HeaderComponent } from './shared/components/header/header.component';
     AppRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule, MatButtonModule,
+    MatOptionModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatToolbarModule,
     FlexLayoutModule,
     CurrencyMaskModule,
-    NgChartsModule,
-    MatCommonModule
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
